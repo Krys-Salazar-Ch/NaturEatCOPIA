f8948d922ef30b6241da253378e1646289ca1fa4
KaMendez04 <katherynmq@gmail.com>
2024-05-22 10:28:43 -0600

    DbContext

---
+++ Services/MyBbContext/MyContext.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.MyBbContext
+{
+    public class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("MyDatabase");
+        }
+        public DbSet<Categories> categories { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Order_Confirmation> orders_confirmations { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>()
+                .HasOne(book => book.Author)
+                .WithMany(author => author.Books);
+        }*/
+    }
+}
