77102e5a9acddb423fe633776b69ba69da285c89
KaMendez04 <katherynmq@gmail.com>
2024-05-21 20:40:40 -0600

    controllers

--- Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net7.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+701d28f77e37dc9faab8e849d1eb60e0e73627c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
